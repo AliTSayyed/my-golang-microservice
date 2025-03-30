e18daaf7dc0273bd54007576fcfa21dcd1a6aa93
ATS <alitsayyed@gmai.com>
2025-03-30 17:01:19 -0500

    basic chi router set up

--- main.go
+++ main.go
@@ -3,12 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	// create a chi router, chi makes routing easy
+	router := chi.NewRouter()
+
+	// the use method allows use to have a function wrap the handler function before executing (middleware)
+	router.Use(middleware.Logger)
+
+	// a router just needs a path and a handler function
+	router.Get("/hello", basicHandler)
+
+	// still use the http.Server struct but the handler now uses the rotuer
 	server := &http.Server{
 		Addr:    ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
 	err := server.ListenAndServe()

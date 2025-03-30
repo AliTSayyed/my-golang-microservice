f3b1270225052f533a9e8aaf4a775613a369cbfc
alitsayyed <alitsayyed@gmai.com>
2025-03-30 18:11:05 -0500

    seperated router and applicaiton

--- main.go
+++ main.go
@@ -1,36 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/alitsayyed/my-golang-microservice/application"
 )
 
 func main() {
-	// create a chi router, chi makes routing easy
-	router := chi.NewRouter()
-
-	// the use method allows use to have a function wrap the handler function before executing (middleware)
-	router.Use(middleware.Logger)
-
-	// a router just needs a path and a handler function
-	router.Get("/hello", basicHandler)
-
-	// still use the http.Server struct but the handler now uses the rotuer
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: router,
-	}
-
-	err := server.ListenAndServe()
+	app := application.New()
+	err := app.Start(context.TODO())
 	if err != nil {
-		fmt.Println("Server failed to run", err)
+		fmt.Println("Failed to start app:", err)
 	}
-
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
 }
